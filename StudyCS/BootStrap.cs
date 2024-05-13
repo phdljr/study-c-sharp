6b6a08085d1cb7cb58b445d71480eda98015b747
시스템개발1 <1@>
2024-05-13 17:42:00 +0900

    초기 커밋.

---
+++ StudyCS/BootStrap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace StudyCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet ds = new DataSet();
+            const string str = "Server=localhost,3306;Database=test;Uid=root;Pwd=1234;";
+            using (MySqlConnection conn = new MySqlConnection(str))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO member VALUES (null, 'Tom')", conn);
+                cmd.ExecuteNonQuery();
+
+                string sql = "SELECT * FROM member";
+                MySqlDataAdapter adpt = new MySqlDataAdapter(sql, conn);
+                adpt.Fill(ds);
+            }
+
+            foreach (DataRow r in ds.Tables[0].Rows)
+            {
+                Console.WriteLine(r["Name"]);
+            }
+        }
+    }
+}
---
+++ StudyCS/Database.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCS
+{
+    internal class Database
+    {
+        public Database()
+        {
+            DataSet ds = new DataSet();
+            const string str = "Server=localhost,3306;Database=test;Uid=root;Pwd=1234;";
+            using (MySqlConnection conn = new MySqlConnection(str))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO member VALUES (null, 'Tom')", conn);
+                cmd.ExecuteNonQuery();
+
+                string sql = "SELECT * FROM member";
+                MySqlDataAdapter adpt = new MySqlDataAdapter(sql, conn);
+                adpt.Fill(ds);
+            }
+
+            foreach (DataRow r in ds.Tables[0].Rows)
+            {
+                Console.WriteLine(r["Name"]);
+            }
+        }
+    }
+}
