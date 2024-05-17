70a08dcfe0e4ac3daa8b6fef8d38dd3a167ea19e
phdljr <phdljr@naver.com>
2024-05-17 10:52:57 +0900

    Feat: LINQ 학습

--- StudyLINQ2SQL/Program.cs
+++ StudyLINQ2SQL/Program.cs
@@ -1,4 +1,23 @@
-﻿public class Program
+﻿namespace MyProgram
 {
 
+    public class Program
+    {
+        static void Main()
+        {
+            int[] numbers = [0,1,2,3,4,5];
+
+            var numQuery = from num in numbers
+                           where num % 2 == 0
+                           orderby num descending
+                           select num;
+
+            var numQuery2 = numbers.Where(num => num % 2 == 0).OrderBy(num => -num);
+
+            foreach (int i in numQuery2)
+            {
+                Console.Write("{0, 5}", i);
+            }
+        }
+    }
 }
